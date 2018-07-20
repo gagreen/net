9447c4a3965245d950961aed7d710c30ed48335f
minseong <37841755+gagreen@users.noreply.github.com>
2018-07-20 13:42:29 +0900

    Update client.py

--- client.py
+++ client.py
@@ -2,6 +2,10 @@ import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(('52.78.104.59', 12345))
-sock.send('hello'.encode())
+
+inp = input()
+
+sock.send(inp.encode())
 data = sock.recv(65535)
+
 print(data.decode())
